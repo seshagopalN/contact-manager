1748830b93616f9d46071676be89ebb1fc926ea1
seshagopalN <seshagopalnarayanan@gmail.com>
2025-09-15 14:49:17 +0530

    fix: remove unused scripts and clean up Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
-    chunkSizeWarningLimit: 1000,
-    rollupOptions:{
-      output:{
-        manualChunks:{
-          react:['react','react-dom']
-        },
-      }
-    }
-  },
-  resolve:{
-    alias:{
-      "@": '/src',
-    }
-  }
 })
