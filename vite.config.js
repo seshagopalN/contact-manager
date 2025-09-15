b573ef856507eecb8dd4e2782fac8a5ddaa7787b
seshagopalN <seshagopalnarayanan@gmail.com>
2025-09-15 14:37:55 +0530

    fix: restructure Vite configuration for build and resolve options

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    chunkSizeWarningLimit: 1000,
+    rollupOptions:{
+      output:{
+        manualChunks:{
+          react:['react','react-dom']
+        },
+      }
+    }
+  },
+  resolve:{
+    alias:{
+      "@": '/src',
+    }
+  }
 })
